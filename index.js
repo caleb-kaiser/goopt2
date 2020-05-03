5763529e84ad6d6c6ca4db4db7d3c86f7461bd1c
Caleb Kaiser <caleb.kaiser.js@gmail.com>
2020-05-03 18:41:04 -0400

    Update for ELB

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ app.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
  });
 
-app.listen(8000, err => {
+app.listen(8080, err => {
   if (err) {
     console.log(err);
   } else {
-    console.log('api is running on 8000');
+    console.log('api is running on 8080');
   }
 });
